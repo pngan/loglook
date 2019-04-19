77fb5fa12dd83f095703657dbba038cec75a08f1
Phillip Ngan <phillip.ngan@gmail.com>
2019-04-19 17:03:49 +1200

    Added vertical position cursor to read off the time

--- loglook/ViewModel/GraphViewModel.cs
+++ loglook/ViewModel/GraphViewModel.cs
@@ -2,8 +2,10 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using LiveCharts;
 using LiveCharts.Configurations;
+using LiveCharts.Helpers;
 using LiveCharts.Wpf;
 using Model;
 
@@ -13,6 +15,8 @@ namespace ViewModel
     {
         private readonly IFileModel m_fileModel;
         private int? m_lineCount;
+        private double m_xPointer;
+        private double m_yPointer;
 
         public GraphViewModel(IFileModel fileModel)
         {
@@ -20,6 +24,7 @@ namespace ViewModel
             m_fileModel.OnSeriesAddedOrChanged += FileModelOnOnSeriesAddedOrChanged;
 
             InspectDataCommand = new RelayCommand(InspectData);
+            MouseMoveCommand = new RelayCommand(DoMouseMove);
 
             Formatter = value => new DateTime((long) (value * TimeSpan.FromHours(1).Ticks)).ToString("t");
 
@@ -31,7 +36,35 @@ namespace ViewModel
             CreateSeries("");
         }
 
+        private void DoMouseMove(object obj)
+        {
+            var chart = (LiveCharts.Wpf.CartesianChart)obj;
+            Point mouseCoordinate = Mouse.GetPosition(chart);
+
+            ////lets get where the mouse is at our chart
+            //var mouseCoordinate = e.GetPosition(chart);
+
+            //now that we know where the mouse is, lets use
+            //ConverToChartValues extension
+            //it takes a point in pixes and scales it to our chart current scale/values
+            var p = chart.ConvertToChartValues(mouseCoordinate);
+
+            //in the Y section, lets use the raw value
+            YPointer = p.Y;
+
+            //for X in this case we will only highlight the closest point.
+            //lets use the already defined ClosestPointTo extension
+            //it will return the closest ChartPoint to a value according to an axis.
+            //here we get the closest point to p.X according to the X axis
+            var series = chart.Series[0];
+            var closetsPoint = series.ClosestPointTo(p.X, AxisOrientation.X);
+
+            XPointer = closetsPoint.X;
+
+        }
+
         public RelayCommand InspectDataCommand { get; }
+        public RelayCommand MouseMoveCommand { get; }
 
         public Func<double, string> Formatter { get; set; }
         public void ToggleSeriesVisibility(int index)
@@ -42,6 +75,18 @@ namespace ViewModel
             series.Visibility = series.Visibility == Visibility.Visible
                 ? Visibility.Hidden
                 : Visibility.Visible;
+        }
+
+        public double XPointer
+        {
+            get => m_xPointer;
+            set => SetField(ref m_xPointer, value);
+        }
+
+        public double YPointer
+        {
+            get => m_yPointer;
+            set => SetField(ref m_yPointer, value);
         }
 
         public int LineCount
@@ -108,5 +153,8 @@ namespace ViewModel
 
         Func<double, string> Formatter { get; }
         void ToggleSeriesVisibility(int index);
+
+        double XPointer { get; }
+        double YPointer { get; }
     }
 }
